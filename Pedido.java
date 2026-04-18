d5de500ca49c259f75e4006b0935f579cc0354b8
BlackSteel-Byte <brayanrp2001@gmail.com>
2026-04-18 13:58:15 +0100

    Update Pedido.java
    Se le ha añadido un breve comentario

--- Pedido.java
+++ Pedido.java
@@ -2,7 +2,7 @@ public class Pedido {
     
     public void procesarPedido(double precioUnidad, int cantidad) {
         double totalCompra = precioUnidad * cantidad;
-        
+                                                                         // Cambios de refactorización en la rama de "Desarrollo"
         totalCompra = aplicarDescuento(totalCompra);
         
         System.out.println("El precio final del pedido es: " + totalCompra);
